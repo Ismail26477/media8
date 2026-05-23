5fa5b7e7831d3d5c16e93e473fa7672f97f246a6
Ismail Sheikh <129026460+Ismail26477@users.noreply.github.com>
2026-05-23 14:01:01 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,12 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    hmr: {
+    hmr: mode === "development" ? {
       overlay: false,
-    },
+      protocol: "ws",
+      host: "localhost",
+      port: 8080,
+    } : false,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
